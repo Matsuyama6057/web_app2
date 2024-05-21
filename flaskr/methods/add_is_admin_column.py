3bce500e3cb5f8997fc848e08fff01fa4eed2906
hanepoyoyo <tsubasa20030121@gmail.com>
2024-05-22 00:57:21 +0900

    DB修正等

--- flaskr/methods/add_is_admin_column.py
+++
@@ -1,5 +0,0 @@
-from db import add_is_admin_column
-
-if __name__ == "__main__":
-    add_is_admin_column()
-    print("is_admin カラムが追加されました。")
--- flaskr/methods/set_user_as_admin.py
+++ flaskr/methods/set_user_as_admin.py
@@ -1,3 +1,11 @@
+import sys
+import os
+
+# 1つ上の階層のパスを取得
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+# 1つ上の階層のパスをsys.pathに追加
+sys.path.append(parent_dir)
+
 from db import get_connection
 
 def set_user_as_admin(username):
